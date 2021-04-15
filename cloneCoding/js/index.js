6118d127029d91ba0bec4d75a1cfc634cc455560
yejinj <80540445+yejinj@users.noreply.github.com>
2021-04-15 15:29:35 +0900

    commit to main

--- cloneCoding/js/index.js
+++ cloneCoding/js/index.js
@@ -1,10 +1,7 @@
-// ※ 클래스명 myswiper 사용
+// 슬라이드를 사용하기 위해 myswiper을 초기화한다
 new Swiper('.myswiper', {
 
-	// 슬라이드 속도 설정
-	// 지정하지 않을시 기본값은 300
+	// 슬라이드 속도 설정, 아무것도 없을 시 기본값은 300
 	speed : 1000,
 
 });
-
-<script> function btn(sample){alert(sample);} </script>
